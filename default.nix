7b89b3910eb51a39d698c67d30a424fca7208515
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2018-07-27 14:55:40 +0200

    lemma-overloading only depends on SSR, not the full math-comp.

--- default.nix
+++ default.nix
@@ -5,26 +5,26 @@ let
    if coq-version == "8.8" then pkgs.coq_8_8
    else import (fetchTarball "https://github.com/coq/coq/tarball/${coq-version}") {};
 
- mathcomp =
-   if coq-version == "8.8" then pkgs.coqPackages_8_8.mathcomp
-   else import ./mathcomp.nix { inherit pkgs coq; };
+ ssreflect =
+   if coq-version == "8.8" then pkgs.coqPackages_8_8.ssreflect
+   else import ./ssreflect.nix { inherit pkgs coq; };
 in
 
 pkgs.stdenv.mkDerivation rec {
 
   name = "lemma-overloading";
 
-  buildInputs = [ coq mathcomp ];
+  buildInputs = [ coq ssreflect ];
 
   src = ./.;
 
   makeFlags =
     if coq-version == "8.8" then ""
-    else "COQPATH=${mathcomp}/lib/coq/user-contrib/";
+    else "COQPATH=${ssreflect}/lib/coq/user-contrib/";
 
   installFlags = "COQLIB=$(out)/lib/coq/";
 
   shellHook =
     if coq-version == "8.8" then ""
-    else "export COQPATH=${mathcomp}/lib/coq/user-contrib/$\{COQPATH:+:$COQPATH}";
+    else "export COQPATH=${ssreflect}/lib/coq/user-contrib/$\{COQPATH:+:$COQPATH}";
 }
