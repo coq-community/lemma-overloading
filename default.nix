de4c0a2ae415e5389112fedcc39ee4c6faf98dd1
Vincent Laporte <Vincent.Laporte@fondation-inria.fr>
2018-11-13 15:58:28 +0000

    [default.nix] Use ssreflect description from nixpkgs

--- default.nix
+++ default.nix
@@ -1,30 +1,26 @@
 { pkgs ? (import <nixpkgs> {}), coq-version ? "master" }:
 
 let
- coq =
-   if coq-version == "8.8" then pkgs.coq_8_8
-   else import (fetchTarball "https://github.com/coq/coq/tarball/${coq-version}") {};
-
- ssreflect =
-   if coq-version == "8.8" then pkgs.coqPackages_8_8.ssreflect
-   else import ./ssreflect.nix { inherit pkgs coq; };
+  coqPackages =
+    if coq-version == "8.8" then pkgs.coqPackages_8_8 else
+    if coq-version == "8.9" then pkgs.coqPackages_8_9 else
+    pkgs.mkCoqPackages
+      (import (fetchTarball "https://github.com/coq/coq/tarball/${coq-version}") {
+        buildIde = false;
+        buildDoc = false;
+        doInstallCheck = false;
+        coq-version = "8.9";
+      })
+    ;
 in
 
 pkgs.stdenv.mkDerivation rec {
 
   name = "lemma-overloading";
 
-  buildInputs = [ coq ssreflect ];
+  buildInputs = with coqPackages; [ coq ssreflect ];
 
   src = ./.;
 
-  makeFlags =
-    if coq-version == "8.8" then ""
-    else "COQPATH=${ssreflect}/lib/coq/user-contrib/";
-
   installFlags = "COQLIB=$(out)/lib/coq/";
-
-  shellHook =
-    if coq-version == "8.8" then ""
-    else "export COQPATH=${ssreflect}/lib/coq/user-contrib/$\{COQPATH:+:$COQPATH}";
 }
